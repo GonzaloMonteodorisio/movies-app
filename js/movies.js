c4e768b67bfd8ae2b7bff5d1359cd24a1483965d
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2024-05-14 21:46:29 -0300

    Change API

--- js/movies.js
+++ js/movies.js
@@ -2,7 +2,9 @@
 document.addEventListener("DOMContentLoaded" , () => {
     fetchData()
 })
+// http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
 
+/*
 const fetchData = async () => {
     
     try {
@@ -18,31 +20,52 @@ const fetchData = async () => {
         // loadingData(false)
     }
 }
+*/
+
+const fetchData = async () => {
+    
+    try {
+        loadingData(true)
+        
+        const res = await fetch("https://hp-api.onrender.com/api/characters")
+        const data = await res.json()
+        console.info('data: ', data)
+       pintarCard(data)
+        
+    } catch (error) {
+        console.log(error)
+    } finally{
+        loadingData(false)
+    }
+}
 
 
 const pintarCard = data => {
-    const peliculas = document.querySelector(".peliculas")
+    const personajes = document.querySelector(".personajes")
     const templateCard = document.querySelector("#template-card").content
     const fragment = document.createDocumentFragment()
     
-    data.Search.forEach(item => {
-        
+    data.forEach(item => {
         const clone = templateCard.cloneNode(true)
-        clone.querySelector("h3").textContent = item.Title
-        clone.querySelector("p").textContent = item.Year
-        clone.querySelector("img").setAttribute("src", item.Poster)
+        clone.querySelector("h3").textContent = item.name
+        clone.querySelector("img").setAttribute("src", item.image)
+        clone.querySelector("img").setAttribute("alt", item.name)
+        clone.querySelector("img").setAttribute("height", "200px")
+        clone.querySelector("p").textContent = `Casa: ${item.house}`
+        clone.querySelector("span").textContent = `${item.gender === 'male' ? `Actor: ${item.actor}` : `Actriz: ${item.actor}`}`
+
         
-        //guardamos en el fragment para evitar el reflow
+        
         fragment.appendChild(clone)
     });
-    peliculas.appendChild(fragment)
+    personajes.appendChild(fragment)
 }
 
-// const loadingData= (estado)=> {
-//     const loading = document.getElementById("loading")
-//     if(estado){
-//         loading.classList.remove("d-none")
-//     }else{
-//         loading.classList.add("d-none")
-//     }
-// }
+const loadingData= (estado)=> {
+    const loading = document.getElementById("loading")
+    if(estado){
+       loading.classList.remove("d-none")
+   }else{
+       loading.classList.add("d-none")
+   }
+}
