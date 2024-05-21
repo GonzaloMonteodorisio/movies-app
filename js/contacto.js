0b658cda87914a4e63f20e2c5367d54683062a29
gerardo kruger <pacokruger1986@gmail.com>
2024-05-20 21:50:35 -0300

    create page contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const selectCasa = document.getElementById('casa');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const selectCasa = document.getElementById('casa');
 
-    // Función para obtener las casas de Hogwarts desde la API
-    fetch('https://www.potterapi.com/v1/houses?key=$2a$10$...')
-        .then(response => response.json())
-        .then(data => {
-            console.log(fetch)
-            data.forEach(house => {
-                const option = document.createElement('option');
-                option.value = house.name.toLowerCase();
-                option.textContent = house.name;
-                selectCasa.appendChild(option);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-});
+//     // Función para obtener las casas de Hogwarts desde la API
+//     fetch('https://www.potterapi.com/v1/houses?key=$2a$10$...')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(fetch)
+//             data.forEach(house => {
+//                 const option = document.createElement('option');
+//                 option.value = house.name.toLowerCase();
+//                 option.textContent = house.name;
+//                 selectCasa.appendChild(option);
+//             });
+//         })
+//         .catch(error => console.error('Error:', error));
+// });
